d2299c623aa57109255f1a8ddf3a758076f47ddf
hasantayyar <tayyar.besik@gmail.com>
2014-05-24 13:00:43 +0300

    wtf

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,34 @@
 $(document).ready(function(){
-	$('.mood button').click(function(){
-		var mood = $(this).html();
-		document.location.href = '/rand/'+mood;
-		if(typeof ga !== "undefined"){
-			ga('send', 'event', 'mood', 'click_mainpage', encodeURIComponent(mood)); 
-		}
-	});
+	
 
+function displayError(){}
+			function displayPosition(position) {
+			  $.post("/",{lat:position.coords.latitude,lon:position.coords.longitude},
+			  	function(data){
+			  		dataObj = JSON.parse(data);
+			  		console.log(dataObj);
+                                        desc = dataObj.weather[0].description;
+                                        if(desc.search('rain')>0){
+                                            $("body").addClass('hell');$("h1").addClass('hell');
+                                            $("h1").html("Hell Yes!"); 
+                                        }else{
+                                            $("body").addClass('well');$("h1").addClass('well');
+                                            $("h1").html("No!"); 
+                                        }
+                                        $("#desc").html(desc); 
+			  	})
+			}
 
-$('#randagain').click(function(){
-		var mood = $(this).attr("rel");
-		document.location.href = '/rand/'+mood;
-		if(typeof ga !== "undefined"){
-			ga('send', 'event', 'mood', 'click_songpage', encodeURIComponent(mood)); 
-		}
-	});
-	 
-})
+	if (navigator.geolocation) {
+			  var timeoutVal = 10 * 1000 * 1000;
+			  navigator.geolocation.getCurrentPosition(
+			    displayPosition, 
+			    displayError,
+			    { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+			  );
+			}
+			else {
+			  alert("Geolocation is not supported by this browser");
+			}
+});
+
